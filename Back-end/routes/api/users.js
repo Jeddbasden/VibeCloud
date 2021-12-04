4e2c71142a14bbe58534a39f09ed4ee78a630a5c
Jeddbasden <jeddbasden2@gmail.com>
2021-12-04 16:39:48 -0600

    sign up

--- Back-end/routes/api/users.js
+++ Back-end/routes/api/users.js
@@ -1,5 +1,23 @@
-const express = require('express');
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
 const router = express.Router();
 
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    console.log(email)
+    const user = await User.signup({ email, username, password });
 
-module.exports = router
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  })
+);
+module.exports = router;
