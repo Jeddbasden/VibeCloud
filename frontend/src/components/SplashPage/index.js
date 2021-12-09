cf76f4f01d262f2724561f30cd46abbc3d17b904
Jeddbasden <jeddbasden2@gmail.com>
2021-12-08 19:21:07 -0600

    pushing to heroku

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,6 +1,22 @@
 import "./SplashPage.css"
+import { NavLink, Redirect } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import * as sessionActions from "../../store/session";  
 
 const SplashPage = () => {
+  const dispatch = useDispatch()
+
+  const sessionUser = useSelector((state) => state.session.user);
+  if (sessionUser) return <Redirect to="/home" />;
+
+  const handleDemoSubmit = (e) => {
+    e.preventDefault();
+
+    const credential = "Demo-lition";
+    const password = "password";
+    return dispatch(sessionActions.login({ credential, password }));
+  }
+
   return (
     <div id="splashContent">
       <div className="splashDiv">
@@ -9,17 +25,31 @@ const SplashPage = () => {
           <h2>Lets Vibe</h2>
         </div>
         <div className="splashBtn">
-          <button>Log in</button>
+          <button>
+            <NavLink className="link" to="/login">
+              Log In
+            </NavLink>
+          </button>
         </div>
         <div className="splashBtn">
-          <button>Sign Up</button>
+          <button>
+            <NavLink className="link" to="/signup">
+              Sign Up
+            </NavLink>
+          </button>
         </div>
         <div className="splashBtn">
-          <button>Demo User</button>
+          <button
+            onClick={(e) => {
+              handleDemoSubmit(e);
+            }}
+          >
+            Demo User
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default SplashPage
