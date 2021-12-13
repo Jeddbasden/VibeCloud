b3f4d2ffae529bef66b600b8a64ea3672cb595e8
Jeddbasden <jeddbasden2@gmail.com>
2021-12-12 18:04:29 -0600

    final styling

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -30,7 +30,7 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
+      <button className="profileBtn" onClick={openMenu}>
         <i className="fas fa-user-circle" />
       </button>
       {showMenu && (
