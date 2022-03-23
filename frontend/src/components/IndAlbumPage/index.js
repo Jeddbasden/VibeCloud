b41cfb135081d6f00403d14396e2590de9a573e7
Jeddbasden <jeddbasden2@gmail.com>
2022-03-22 23:42:28 -0500

    started album page

--- frontend/src/components/IndAlbumPage/index.js
+++ frontend/src/components/IndAlbumPage/index.js
@@ -1,9 +1,8 @@
 import React, { useEffect } from "react";
+import { Dispatch } from "react";
 
 const IndAlbumPage = () => {
-  return (
-    <h1>YO!!!!</h1>
-  )
-}
+  return <h1>YO!!!!</h1>;
+};
 
 export default IndAlbumPage;
